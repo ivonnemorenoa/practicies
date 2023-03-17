fc14fb0628443493d61200672a15176a2f4ae690
ivonnemoreno <ivonne.morenoa@hotmail.com>
2023-03-16 23:48:25 -0600

    test case color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'qa-practicies';
   contador:number=0;
+  active:boolean=false;
+  
+
+
   public incrementar (): void{
     this.contador=this.contador+1;
     //this.contador++;
     
+  }
+
+
+  public colorear (): void{
+
+    // this.active = !this.active;
+
+    if(this.active){
+      this.active=false;
+
+    }else {
+      this.active=true;
+    }
+
+
   }
 }
 
